3e77ccc9f074597be0461bf6f762118cbe561d4e
Harry Vu <aki@moekawaii.space>
2025-05-05 11:15:18 -0400

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,6 @@
 oh-my-posh init pwsh --config 'C:\Users\aryad\AppData\Local\Programs\oh-my-posh\themes\nordcustom.omp.json' | Invoke-Expression
-import-Module -Name Terminal-Icons
+Import-Module -Name Terminal-Icons
 Import-Module PSReadLine
 Set-PSReadLineOption -PredictionSource History
-set-psreadlineoption -PredictionViewStyle ListView
+Set-PSReadLineOption -PredictionViewStyle ListView
                          
